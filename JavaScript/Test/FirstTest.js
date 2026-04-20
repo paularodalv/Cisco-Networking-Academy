7a61096f24ffe7579016d539067e7bed7ad8c183
Paula <paularodalv06@gmail.com>
2026-04-20 22:54:36 +0200

    Creación de repositorio y añadido el T1

---
+++ JavaScript/Test/FirstTest.js
@@ -0,0 +1,23 @@
+// Question 1: Use console.log to output your full name to the console.
+console.log("Hello, world!"); 
+
+//Question 2: Output your year of birth.
+console.log("12/07/2006");
+
+//Question 3: Try again to output your year of birth, this time passing the date without the quotes.
+console.log(12072006);
+
+//Question 4: We can pass several arguments to console.log separated by commas, e.g.:
+console.log("Paula Rodriguez (2006)");
+console.log("Paula", "Rodriguez", "(2006)");
+
+//Question 5: Output the same information (name, surname, year) to the console, not side by side, but on consecutive lines.
+console.log("Paula");
+console.log("Rodriguez");
+console.log("2006");
+
+//Question 6: A string can be concatenated using the + sign, for example "abc" + "def" will be treated as "abcdef". Try writing your name, surname, and year of birth on one line again, this time not separated by commas, but by + signs.
+console.log("Paula" + "Rodriguez" + "(2006)");
+
+//Question 7: Put spaces in the appropriate places, so that when displayed, you get the same effect as in Question 4.
+console.log("Paula " + "Rodriguez " + "(2006)");
